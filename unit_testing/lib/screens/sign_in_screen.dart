115bce52125140c004daaf813d4576fbeda63f31
Rudrank Riyam <rudrankriyam@gmail.com>
2022-11-20 06:03:26 +0530

    Replace RaisedButton with ElevatedButton

--- unit_testing/lib/screens/sign_in_screen.dart
+++ unit_testing/lib/screens/sign_in_screen.dart
@@ -43,7 +43,7 @@ class _SignInScreenState extends State<SignInScreen> {
               SizedBox(
                 height: 50,
               ),
-              RaisedButton(
+              ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState.validate()) {
                     Navigator.of(context).pushReplacementNamed('/homeScreen');
